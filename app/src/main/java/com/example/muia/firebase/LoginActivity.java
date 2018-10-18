6a5a0b9eeb95334d01b4d4f52105a9b0ddf09d0f
mchlmuia <mikepeter18@gmail.com>
2018-10-18 13:39:04 +0300

    My Commit

--- app/src/main/java/com/example/muia/firebase/LoginActivity.java
+++ app/src/main/java/com/example/muia/firebase/LoginActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     FirebaseAuth mAuth;
